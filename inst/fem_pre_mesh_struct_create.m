8d1d13aaedaa89cf5ff20ebae048778938441cd5
Reinhard <octave-user@a1.net>
2020-08-15 18:35:38 +0200

    Add new test case

--- inst/fem_pre_mesh_struct_create.m
+++ inst/fem_pre_mesh_struct_create.m
@@ -1506,7 +1506,7 @@ endfunction
 %! material.rho = 0.000751 * 4.4482 / (25.4e-3^4);
 %! geo.D = 2 * 4.4688 * 25.4e-3;
 %! geo.t = 0.0625 * 25.4e-3;
-%! N = 100;
+%! N = 39;
 %!
 %! function [x, y, z] = sphere_geo(geo, r, s, t)
 %!   R = 0.5 * geo.D - r * geo.t;
@@ -1532,8 +1532,8 @@ endfunction
 %!   fref = [5078, 6005, 6378, 6729];
 %!
 %!   geometry.mesh_size.r = linspace(-0.5, 0.5, 2);
-%!   geometry.mesh_size.s = linspace(-90 * pi / 180, 90 * pi / 180, 72);
-%!   geometry.mesh_size.t = linspace(-180 * pi / 180, 180 * pi / 180, 72);
+%!   geometry.mesh_size.s = linspace(-90 * pi / 180, 90 * pi / 180, 36);
+%!   geometry.mesh_size.t = linspace(-180 * pi / 180, 180 * pi / 180, 36);
 %!   load_data.pressure = 1;
 %!   geometry.sewing.tolerance = sqrt(eps) * 0.5 * geo.D;
 %!   geometry.spatial_coordinates = @(r, s, t) feval("sphere_geo", geo, r, s, t);
@@ -1556,7 +1556,11 @@ endfunction
 %!                               load_case);
 %!   options.number_of_threads = int32(4);
 %!   shift = sqrt(eps) * max(abs(diag(mat_ass.K))) / max(abs(diag(mat_ass.M)));
-%!   sol_eig = fem_sol_modal(mesh, dof_map, mat_ass, N, shift);
+%!   tol = eps^0.4;
+%!   alg = "shift-invert";
+%!   solver = "pastix";
+%!   num_threads = 4;
+%!   sol_eig = fem_sol_modal(mesh, dof_map, mat_ass, N, shift, tol, alg, solver, num_threads);
 %!   sol_eig.stress = fem_ass_matrix(mesh, dof_map, [FEM_SCA_STRESS_VMIS], load_case, sol_eig);
 %!   opts.scale_def = 0.25 * geo.D / max(max(max(abs(sol_eig.def))));
 %!   opts.print_and_exit = true;
@@ -1567,10 +1571,116 @@ endfunction
 %!     opts.print_to_file = tempname();
 %!     opts.rotation_angle = [-pi/2, 0, 0];
 %!     fem_post_sol_external(mesh, sol_eig, opts);
-%!     [img, map, alpha] = imread([opts.print_to_file, "_001.jpg"]);
-%!     figure("visible", "off");
-%!     imshow(img, map);
-%!     title("deformed mesh %s / Van Mises stress");
+%!     for i=7:N
+%!       [img, map, alpha] = imread(sprintf("%s_%03d.jpg", opts.print_to_file, i));
+%!       figure("visible", "off");
+%!       imshow(img, map);
+%!       title(sprintf("mode %d: %.0fHz", i, sol_eig.f(i)));
+%!     endfor
+%!   unwind_protect_cleanup
+%!     if (numel(opts.print_to_file))
+%!       unlink([opts.print_to_file, "_001.jpg"]);
+%!     endif
+%!   end_unwind_protect
+%! assert(sol_eig.f([7, 12, 19, 39]), fref, 5e-4 * max(fref));
+%! figure_list();
+
+%!demo
+%! ## distorted sphere
+%! close all;
+%! material.E = 28e6 * 6895;
+%! material.nu = 0.28;
+%! material.rho = 0.000751 * 4.4482 / (25.4e-3^4);
+%! geo.a = 2 * 4.4688 * 25.4e-3;
+%! geo.b = 1 * 4.4688 * 25.4e-3;
+%! geo.c = 1.5 * 4.4688 * 25.4e-3;
+%! geo.t = 0.625 * 25.4e-3;
+%! geo.Psii = [-180, -135, -90, -45, 0,  45, 90, 135, 180] * pi / 180;
+%! geo.Psir = [   1,  1.1,   1, 1.1, 1, 1.1,  1, 1.1,   1];
+%! geo.Zetai = [-180, -135, -90, -45, 0,  45, 90, 135, 180] * pi / 180;
+%! geo.Zetar = [   1,  1.1,   1, 1.1, 1, 1.1,  1, 1.1,   1];
+%! geo.Psii = [geo.Psii(1:end - 1) - 2 * pi, geo.Psii, geo.Psii(2:end) + 2 * pi];
+%! geo.Psir = [geo.Psir(1:end - 1), geo.Psir, geo.Psir(2:end)];
+%! geo.Zetai = [geo.Zetai(1:end - 1) - 2 * pi, geo.Zetai, geo.Zetai(2:end) + 2 * pi];
+%! geo.Zetar = [geo.Zetar(1:end - 1), geo.Zetar, geo.Zetar(2:end)];
+%! N = 39;
+%!
+%! function [x, y, z] = sphere_geo(geo, r, s, t)
+%!   Zeta = s;
+%!   Psi = t;
+%!   Psir = interp1(geo.Psii, geo.Psir, Psi, "pchip");
+%!   Zetar = interp1(geo.Zetai, geo.Zetar, Zeta, "pchip");
+%!   xo = Psir * geo.a * cos(Zeta) * cos(Psi);
+%!   yo = Psir * geo.b * cos(Zeta) * sin(Psi);
+%!   zo = Zetar * geo.c * sin(Zeta);
+%!   xi = (Psir * geo.a - geo.t) * cos(Zeta) * cos(Psi);
+%!   yi = (Psir * geo.b - geo.t) * cos(Zeta) * sin(Psi);
+%!   zi = (Zetar * geo.c - geo.t) * sin(Zeta);
+%!   x = xo + (xi - xo) * r;
+%!   y = yo + (yi - yo) * r;
+%!   z = zo + (zi - zo) * r;
+%! endfunction
+%!
+%! function [F, locked] = sphere_bound(r, s, t, geo, load_data)
+%!   F = [];
+%!   locked = false(1, 3);
+%! endfunction
+%!
+%! function p = sphere_pressure(r, s, t, geo, load_data, perm_idx)
+%!   p = [];
+%!   if (r == 1)
+%!     p(perm_idx) = load_data.pressure;
+%!   endif
+%! endfunction
+%!
+%!   fref = [5078, 6005, 6378, 6729];
+%!
+%!   geometry.mesh_size.r = linspace(-0.5, 0.5, 2);
+%!   geometry.mesh_size.s = linspace(-90 * pi / 180, 90 * pi / 180, 36);
+%!   geometry.mesh_size.t = linspace(-180 * pi / 180, 180 * pi / 180, 36);
+%!   load_data.pressure = 1;
+%!   geometry.sewing.tolerance = sqrt(eps) * geo.a;
+%!   geometry.spatial_coordinates = @(r, s, t) feval("sphere_geo", geo, r, s, t);
+%!   geometry.material_selector = @(r, s, t) 1;
+%!   geometry.boundary_condition =  @(r, s, t, geometry, load_data) feval("sphere_bound", r, s, t, geo, load_data);
+%!   geometry.pressure_boundary_condition = @(r, s, t, geometry, load_data, perm_idx) feval("sphere_pressure", r, s, t, geo, load_data, perm_idx);
+%!   options.elem_type = "iso20";
+%!   [mesh, load_case] = fem_pre_mesh_struct_create(geometry, load_data, material, options);
+%!   [dof_map] = fem_ass_dof_map(mesh, load_case);
+%!
+%!   [mat_ass.M, ...
+%!    mat_ass.K, ...		 
+%!    mat_ass.R, ...
+%!    mat_info, ...
+%!    mesh_info] = fem_ass_matrix(mesh, ...
+%!                               dof_map, ...
+%!                               [FEM_MAT_MASS, ...
+%!				  FEM_MAT_STIFFNESS, ...
+%!                                FEM_VEC_LOAD_CONSISTENT], ...
+%!                               load_case);
+%!   options.number_of_threads = int32(4);
+%!   shift = sqrt(eps) * max(abs(diag(mat_ass.K))) / max(abs(diag(mat_ass.M)));
+%!   tol = eps^0.4;
+%!   alg = "shift-invert";
+%!   solver = "pastix";
+%!   num_threads = 4;
+%!   sol_eig = fem_sol_modal(mesh, dof_map, mat_ass, N, shift, tol, alg, solver, num_threads);
+%!   sol_eig.stress = fem_ass_matrix(mesh, dof_map, [FEM_SCA_STRESS_VMIS], load_case, sol_eig);
+%!   opts.scale_def = 0.5 * geo.a / max(max(max(abs(sol_eig.def))));
+%!   opts.print_and_exit = true;
+%!   opts.print_to_file = "";
+%!   opts.skin_only = true;
+%!   opts.show_element = true;
+%!   unwind_protect
+%!     opts.print_to_file = tempname();
+%!     opts.rotation_angle = [-pi/2, 0, 0];
+%!     fem_post_sol_external(mesh, sol_eig, opts);
+%!     for i=7:N
+%!       [img, map, alpha] = imread(sprintf("%s_%03d.jpg", opts.print_to_file, i));
+%!       figure("visible", "off");
+%!       imshow(img, map);
+%!       title(sprintf("mode %d: %.0fHz", i, sol_eig.f(i)));
+%!     endfor
 %!   unwind_protect_cleanup
 %!     if (numel(opts.print_to_file))
 %!       unlink([opts.print_to_file, "_001.jpg"]);
