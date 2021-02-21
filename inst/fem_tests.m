d92b01c0177deae0ae05aac11bbd78d18a781c16
Reinhard <octave-user@a1.net>
2021-02-21 20:32:14 +0100

    Add first test for thermal conductivity matrix

--- inst/fem_tests.m
+++ inst/fem_tests.m
@@ -4174,6 +4174,33 @@
 %!   rand("state", s);
 %! end_unwind_protect
 
+%!test
+%! ## TEST 55
+%! close all;
+%! scale_stat = 1;
+%! scale_eig = 250e-3;
+%! material.E = 210000e6;
+%! material.nu = 0.3;
+%! material.rho = 7850;
+%! material.k = diag([54, 54, 54]);
+%! Fy = 15000;
+%! h = 10e-3 / 2;
+%! geometry.l = 1000e-3;
+%! geometry.w = 10e-3;
+%! geometry.h = 50e-3;
+%! mesh_size.num_elem_l = ceil(geometry.l / h);
+%! mesh_size.num_elem_w = ceil(geometry.w / h);
+%! mesh_size.num_elem_h = ceil(geometry.h / h);
+%! f = zeros(3, 1);
+%! [mesh] = fem_pre_mesh_cube_create(geometry, mesh_size, material, f);
+%! load_case.locked_dof = false(rows(mesh.nodes), 1);
+%! load_case.domain = FEM_DO_THERMAL;
+%! [dof_map] = fem_ass_dof_map(mesh, load_case);
+%! [mat_ass.Kk] = fem_ass_matrix(mesh, ...
+%!                              dof_map, ...
+%!                              [FEM_MAT_THERMAL_COND], ...
+%!                              load_case);
+
 %!demo
 %! ## DEMO 1
 %! ## Cantilever beam with rectangular cross section and lateral load
