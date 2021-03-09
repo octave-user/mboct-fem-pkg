5aa1c93d52375d65d8358a0da39aeed91a37a08f
Reinhard <octave-user@a1.net>
2021-03-09 21:26:28 +0100

    switch to sqp

--- src/__mboct_fem_pkg__.cc
+++ src/__mboct_fem_pkg__.cc
@@ -8304,8 +8304,7 @@ private:
      struct FuncData {
           FuncData(const ColumnVector& Xm, const ColumnVector& Xs)
                :oSNCO(Xm, Xs),
-                //opt(nlopt_create(NLOPT_LD_SLSQP, iNumDir)),
-                opt(nlopt_create(NLOPT_LN_COBYLA, iNumDir)),
+                opt(nlopt_create(NLOPT_LD_SLSQP, iNumDir)),
                 Hf(iNumDimNode, iNumNodesElem * iNumDimNode),
                 rv(iNumDir, 0.),
                 f(iNumDimNode) {
