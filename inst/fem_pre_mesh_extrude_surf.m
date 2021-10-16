257609e2792960ab6d187bb41594df8eef1b80f5
Reinhard <octave-user@a1.net>
2021-10-16 19:36:56 +0200

    Fix invalid use of random numbers; Fix missing rho for singular stiffness matrix

--- inst/fem_pre_mesh_extrude_surf.m
+++ inst/fem_pre_mesh_extrude_surf.m
@@ -105,99 +105,119 @@ endfunction
 
 %!test
 %! ## TEST 1
-%! mesh.nodes = [0.0, 0.0, -1.0, 0.0, 0.0, 0.0;
-%!               1.0, 0.0, -1.0, 0.0, 0.0, 0.0;
-%!               0.0, 1.0, -1.0, 0.0, 0.0, 0.0;
-%!               0.5, 0.0, -1.0, 0.0, 0.0, 0.0;
-%!               0.5, 0.5, -1.0, 0.0, 0.0, 0.0;
-%!               0.0, 0.5, -1.0, 0.0, 0.0, 0.0];
-%! e1 = rand(3, 1);
-%! e2 = rand(3, 1);
-%! e3 = cross(e1, e2);
-%! e2 = cross(e3, e1);
-%! R = [e1, e2, e3];
-%! R /= diag(norm(R, "cols"));
-%! mesh.nodes(:, 1:3) *= R.';
-%! mesh.elements.tria6h = int32(1:6);
-%! mesh.groups.tria6h.id = int32(1);
-%! mesh.groups.tria6h.name = "surface";
-%! mesh.groups.tria6h.elements = int32(1);
-%! mesh.group.tria6h.nodes = unique(mesh.elements.tria6h);
-%! h = 2;
-%! N = 10;
-%! [mesh.nodes, mesh.elements.penta15] = fem_pre_mesh_extrude_surf(mesh, "tria6h", int32(1), repmat(h / N, 1, N));
-%! load_case_dof.locked_dof = false(rows(mesh.nodes), 6);
-%! mesh.materials.penta15 = ones(rows(mesh.elements.penta15), 1, "int32");
-%! mesh.material_data.E = 210000e6;
-%! mesh.material_data.nu = 0.3;
-%! mesh.material_data.rho = 7850;
-%!
-%! dof_map = fem_ass_dof_map(mesh, load_case_dof);
-%! [mat_ass.K, ...
-%!  mat_ass.M, ...
-%!  mat_ass.mtot, ...
-%!  mat_ass.mat_info, ...
-%!  mat_ass.mesh_info] = fem_ass_matrix(mesh, ...
-%!                                      dof_map, ...
-%!                                      [FEM_MAT_STIFFNESS, ...
-%!                                       FEM_MAT_MASS, ...
-%!                                       FEM_SCA_TOT_MASS]);
-%! sol_eig = fem_sol_modal(mesh, dof_map, mat_ass, 20);
-%! mref = mesh.material_data.rho;
-%! rtol = 10 * eps;
-%! assert(mat_ass.mtot, mref, rtol * mref);
-%! assert(all(sol_eig.f(1:6) < 1e-6 * max(sol_eig.f)));
-%! assert(all(sol_eig.f(7:end) > 1e-1 * max(sol_eig.f)));
+%! rndstate = rand("state");
+%! rand("seed", 0);
+%! unwind_protect
+%!   for i=1:50
+%!     mesh.nodes = [0.0, 0.0, -1.0, 0.0, 0.0, 0.0;
+%!                   1.0, 0.0, -1.0, 0.0, 0.0, 0.0;
+%!                   0.0, 1.0, -1.0, 0.0, 0.0, 0.0;
+%!                   0.5, 0.0, -1.0, 0.0, 0.0, 0.0;
+%!                   0.5, 0.5, -1.0, 0.0, 0.0, 0.0;
+%!                   0.0, 0.5, -1.0, 0.0, 0.0, 0.0];
+%!     e1 = rand(3, 1);
+%!     e2 = rand(3, 1);
+%!     e3 = cross(e1, e2);
+%!     e2 = cross(e3, e1);
+%!     R = [e1, e2, e3];
+%!     R /= diag(norm(R, "cols"));
+%!     mesh.nodes(:, 1:3) *= R.';
+%!     mesh.elements.tria6h = int32(1:6);
+%!     mesh.groups.tria6h.id = int32(1);
+%!     mesh.groups.tria6h.name = "surface";
+%!     mesh.groups.tria6h.elements = int32(1);
+%!     mesh.group.tria6h.nodes = unique(mesh.elements.tria6h);
+%!     h = 2;
+%!     N = 10;
+%!     [mesh.nodes, mesh.elements.penta15] = fem_pre_mesh_extrude_surf(mesh, "tria6h", int32(1), repmat(h / N, 1, N));
+%!     load_case_dof.locked_dof = false(rows(mesh.nodes), 6);
+%!     mesh.materials.penta15 = ones(rows(mesh.elements.penta15), 1, "int32");
+%!     mesh.material_data.E = 210000e6;
+%!     mesh.material_data.nu = 0.3;
+%!     mesh.material_data.rho = 7850;
+
+%!     dof_map = fem_ass_dof_map(mesh, load_case_dof);
+%!     [mat_ass.K, ...
+%!      mat_ass.M, ...
+%!      mat_ass.mtot, ...
+%!      mat_ass.mat_info, ...
+%!      mat_ass.mesh_info] = fem_ass_matrix(mesh, ...
+%!                                          dof_map, ...
+%!                                          [FEM_MAT_STIFFNESS, ...
+%!                                           FEM_MAT_MASS, ...
+%!                                           FEM_SCA_TOT_MASS]);
+%!     N = 50;
+%!     rho = 1e-2 * max(max(abs(mat_ass.K))) / max(max(abs(mat_ass.M)));
+%!     sol_eig = fem_sol_modal(mesh, dof_map, mat_ass, N, rho);
+%!     mref = mesh.material_data.rho;
+%!     rtol = sqrt(eps);
+%!     assert(mat_ass.mtot, mref, rtol * mref);
+%!     assert(all(sol_eig.f(1:6) < 1e-4 * max(sol_eig.f)));
+%!     assert(all(sol_eig.f(7:end) > 1e-1 * max(sol_eig.f)));
+%!   endfor
+%! unwind_protect_cleanup
+%!   rand("state", rndstate);
+%! end_unwind_protect
 
 %!test
 %! ## TEST 2
-%! mesh.nodes = [-1, -1, -1, 0, 0, 0;
-%!                1, -1, -1, 0, 0, 0;
-%!                1,  1, -1, 0, 0, 0;
-%!               -1,  1, -1, 0, 0, 0;
-%!                0, -1, -1, 0, 0, 0;
-%!                1,  0, -1, 0, 0, 0;
-%!                0,  1, -1, 0, 0, 0;
-%!               -1,  0, -1, 0, 0, 0;
-%!                0,  0, -1, 0, 0, 0];
-%! e1 = rand(3, 1);
-%! e2 = rand(3, 1);
-%! e3 = cross(e1, e2);
-%! e2 = cross(e3, e1);
-%! R = [e1, e2, e3];
-%! R /= diag(norm(R, "cols"));
-%! mesh.nodes(:, 1:3) *= R.';
-%! mesh.elements.tria6h = int32([1,2,3,5,6,9;
-%!                               1,3,4,9,7,8]);
-%! mesh.groups.tria6h.id = int32(1);
-%! mesh.groups.tria6h.name = "surface";
-%! mesh.groups.tria6h.elements = int32([1, 2]);
-%! mesh.group.tria6h.nodes = unique(mesh.elements.tria6h);
-%! h = 2;
-%! N = 5;
-%! [mesh.nodes, mesh.elements.penta15] = fem_pre_mesh_extrude_surf(mesh, "tria6h", int32(1), repmat(h / N, 1, N));
-%! load_case_dof.locked_dof = false(rows(mesh.nodes), 6);
-%! mesh.materials.penta15 = ones(rows(mesh.elements.penta15), 1, "int32");
-%! mesh.material_data.E = 210000e6;
-%! mesh.material_data.nu = 0.3;
-%! mesh.material_data.rho = 7850;
-%!
-%! dof_map = fem_ass_dof_map(mesh, load_case_dof);
-%! [mat_ass.K, ...
-%!  mat_ass.M, ...
-%!  mat_ass.mtot, ...
-%!  mat_ass.mat_info, ...
-%!  mat_ass.mesh_info] = fem_ass_matrix(mesh, ...
-%!                                      dof_map, ...
-%!                                      [FEM_MAT_STIFFNESS, ...
-%!                                       FEM_MAT_MASS, ...
-%!                                       FEM_SCA_TOT_MASS]);
-%! sol_eig = fem_sol_modal(mesh, dof_map, mat_ass, 20);
-%! mref = 8 * mesh.material_data.rho;
-%! rtol = 10 * eps;
-%! assert(mat_ass.mtot, mref, rtol * mref);
-%! assert(all(sol_eig.f(1:6) < 1e-6 * max(sol_eig.f)));
-%! assert(all(sol_eig.f(7:end) > 1e-1 * max(sol_eig.f)));
+%! rndstate = rand("state");
+%! unwind_protect
+%!   rand("seed", 0);
+%!   for i=1:50
+%!     mesh.nodes = [-1, -1, -1, 0, 0, 0;
+%!                   1, -1, -1, 0, 0, 0;
+%!                   1,  1, -1, 0, 0, 0;
+%!                   -1,  1, -1, 0, 0, 0;
+%!                   0, -1, -1, 0, 0, 0;
+%!                   1,  0, -1, 0, 0, 0;
+%!                   0,  1, -1, 0, 0, 0;
+%!                   -1,  0, -1, 0, 0, 0;
+%!                   0,  0, -1, 0, 0, 0];
+%!     e1 = rand(3, 1);
+%!     e2 = rand(3, 1);
+%!     e3 = cross(e1, e2);
+%!     e2 = cross(e3, e1);
+%!     R = [e1, e2, e3];
+%!     R /= diag(norm(R, "cols"));
+%!     mesh.nodes(:, 1:3) *= R.';
+%!     mesh.elements.tria6h = int32([1,2,3,5,6,9;
+%!                                   1,3,4,9,7,8]);
+%!     mesh.groups.tria6h.id = int32(1);
+%!     mesh.groups.tria6h.name = "surface";
+%!     mesh.groups.tria6h.elements = int32([1, 2]);
+%!     mesh.group.tria6h.nodes = unique(mesh.elements.tria6h);
+%!     h = 2;
+%!     N = 5;
+%!     [mesh.nodes, mesh.elements.penta15] = fem_pre_mesh_extrude_surf(mesh, "tria6h", int32(1), repmat(h / N, 1, N));
+%!     load_case_dof.locked_dof = false(rows(mesh.nodes), 6);
+%!     mesh.materials.penta15 = ones(rows(mesh.elements.penta15), 1, "int32");
+%!     mesh.material_data.E = 210000e6;
+%!     mesh.material_data.nu = 0.3;
+%!     mesh.material_data.rho = 7850;
+
+%!     dof_map = fem_ass_dof_map(mesh, load_case_dof);
+%!     [mat_ass.K, ...
+%!      mat_ass.M, ...
+%!      mat_ass.mtot, ...
+%!      mat_ass.mat_info, ...
+%!      mat_ass.mesh_info] = fem_ass_matrix(mesh, ...
+%!                                          dof_map, ...
+%!                                          [FEM_MAT_STIFFNESS, ...
+%!                                           FEM_MAT_MASS, ...
+%!                                           FEM_SCA_TOT_MASS]);
+%!     N = 50;
+%!     rho = 1e-2 * max(max(abs(mat_ass.K))) / max(max(abs(mat_ass.M)));
+%!     sol_eig = fem_sol_modal(mesh, dof_map, mat_ass, N, rho);
+%!     mref = 8 * mesh.material_data.rho;
+%!     rtol = 10 * eps;
+%!     assert(mat_ass.mtot, mref, rtol * mref);
+%!     assert(all(sol_eig.f(1:6) < 1e-6 * max(sol_eig.f)));
+%!     assert(all(sol_eig.f(7:end) > 1e-1 * max(sol_eig.f)));
+%!   endfor
+%! unwind_protect_cleanup
+%!   rand("state", rndstate);
+%! end_unwind_protect
 
 %!test
 %! ### TEST 3
@@ -790,7 +810,7 @@ endfunction
 %!            + 1j * omega * complex(mat_ass.Dfs_re, mat_ass.Dfs_im) ...
 %!            + complex(mat_ass.Kfs_re, mat_ass.Kfs_im);
 %!       Reff = complex(mat_ass.Rfs(:, 1), mat_ass.Rfs(:, 2));
-%!       opt_sol.number_of_threads = int32(6);
+%!       opt_sol.number_of_threads = int32(4);
 %!       opt_sol.solver = "pastix";
 %!       opt_sol.compress_when = int32(0);
 %!       opt_sol.compress_min_ratio = 1;
