ef9dae5fce25bac847601c2a094a9fdf4cdf7762
Reinhard <octave-user@a1.net>
2023-07-25 21:03:00 +0200

    Increase mesh size; add check for result of test1

--- inst/fem_cms_export.m
+++ inst/fem_cms_export.m
@@ -233,17 +233,14 @@ endfunction
 %! SI_unit_kilogram = 1e-3;
 %! SI_unit_newton = SI_unit_kilogram * SI_unit_meter / SI_unit_second^2;
 %! SI_unit_pascal = SI_unit_newton / SI_unit_meter^2;
-
 %! param.alpha = 0 / (1 / SI_unit_second);
 %! param.beta = 1e-7 / (SI_unit_second);
-
-%! param.h = 2.5e-3 / SI_unit_meter; ## mesh size
+%! param.h = 4e-3 / SI_unit_meter; ## mesh size
 %! param.l = 350e-3 / SI_unit_meter; ## bearing distance
 %! param.d = 10e-3 / SI_unit_meter; ## shaft diameter
 %! param.D = 150e-3 / SI_unit_meter; ##disk diameter
 %! param.w = 15e-3 / SI_unit_meter; ## disk width
 %! param.o = 75e-3 / SI_unit_meter; ## disk offset
-
 %! param.ecg = 1e-3 * param.D;
 %! param.dm = 1e-6 / SI_unit_kilogram;
 %! param.E = 210000e6 / SI_unit_pascal;
@@ -254,7 +251,6 @@ endfunction
 %! param.omega0 = 1000 * pi / 30 / (1 / SI_unit_second);
 %! param.omega1 = 10000 * pi / 30 / (1 / SI_unit_second);
 %! param.n = 10;
-
 %! cms_opt.algorithm = "shift-invert";
 %! cms_opt.refine_max_iter = int32(3);
 %! cms_opt.element.name = "elem_id_rotor";
@@ -270,7 +266,6 @@ endfunction
 %! options.mbdyn_command = "mbdyn";
 %! options.f_run_mbdyn = [true, true];
 %! options.verbose = false;
-
 %! function R2j = norm_ref_frame(R2)
 %!   e1 = R2(:, 1);
 %!   e3 = [0; 0; 1];
@@ -278,9 +273,7 @@ endfunction
 %!   e1 = cross(e2, e3);
 %!   R2j = [e1 / norm(e1), e2 / norm(e2), e3 / norm(e3)];
 %! endfunction
-%!
 %! filename = "";
-%!
 %! unwind_protect
 %!   filename = tempname();
 %!   if (ispc())
@@ -297,7 +290,6 @@ endfunction
 %!     if (fd == -1)
 %!       error("failed to open file \"%s\"", mbdyn_filenames{1});
 %!     endif
-
 %!     fputs(fd, "include: \"${MBDYN_ROTOR_DYN_CMS_PARAM_FILE}\";\n");
 %!     fputs(fd, "set: integer ref_id_ground = 1001;\n");
 %!     fputs(fd, "set: integer ref_id_shaft = 1002;\n");
@@ -468,7 +460,6 @@ endfunction
 %!       fclose(fd);
 %!     endif
 %!   end_unwind_protect
-
 %!   fd = -1;
 %!   unwind_protect
 %!     fd = fopen(mbdyn_filenames{2}, "w");
@@ -796,17 +787,13 @@ endfunction
 %!   fd = -1;
 %!   unwind_protect
 %!     [fd, msg] = fopen(geometry_file, "w");
-
 %!     if (fd == -1)
 %!       error("failed to open file \"%s\"", geometry_file);
 %!     endif
-
 %!     fn = fieldnames(param);
-
 %!     for i=1:length(fn)
 %!       fprintf(fd, "%s = %g;\n", fn{i}, getfield(param, fn{i}));
 %!     endfor
-
 %!     fputs(fd, "SetFactory(\"OpenCASCADE\");\n");
 %!     fputs(fd, "Point(1) = {0, 0, -0.5 * l};\n");
 %!     fputs(fd, "Point(2) = {0.5 * d, 0, -0.5 * l};\n");
@@ -832,6 +819,8 @@ endfunction
 %!     fputs(fd, "For i In {0:#B[] - 1}\n");
 %!     fputs(fd, "    Physical Surface(i) = {B[i]};\n");
 %!     fputs(fd, "EndFor\n");
+%!     fputs(fd, "Mesh.HighOrderOptimize = 2;\n");
+%!     fputs(fd, "Mesh.OptimizeThreshold=0.99;\n");
 %!   unwind_protect_cleanup
 %!     if (fd ~= -1)
 %!       fclose(fd);
@@ -847,13 +836,10 @@ endfunction
 %!                        "-ho_max", "3", ...
 %!                        geometry_file, ...
 %!                        "-o", [filename, ".msh"]});
-
 %!   status = spawn_wait(pid);
-
 %!   if (status ~= 0)
 %!     warning("gmsh failed with status %d", status);
 %!   endif
-
 %!   fprintf(stderr, "loading mesh ...\n");
 %!   mesh = fem_pre_mesh_reorder(fem_pre_mesh_import([filename, ".msh"], "gmsh"));
 %!   group_defs(1).id = 1;
@@ -867,7 +853,6 @@ endfunction
 %!   group_defs(1).geometry.ymax = 0.5 * param.d;
 %!   group_defs(1).geometry.zmin = 0;
 %!   group_defs(1).geometry.zmax = 0;
-
 %!   group_defs(2).id = 2;
 %!   group_defs(2).name = "bearing2";
 %!   group_defs(2).R = eye(3);
@@ -879,7 +864,6 @@ endfunction
 %!   group_defs(2).geometry.ymax = 0.5 * param.d;
 %!   group_defs(2).geometry.zmin = 0;
 %!   group_defs(2).geometry.zmax = 0;
-
 %!   group_defs(3).id = 3;
 %!   group_defs(3).name = "rotor";
 %!   group_defs(3).R = eye(3);
@@ -889,10 +873,8 @@ endfunction
 %!   group_defs(3).geometry.rmax = 0.5 * param.D;
 %!   group_defs(3).geometry.zmin = -0.5 * param.w;
 %!   group_defs(3).geometry.zmax = 0.5 * param.w;
-
 %!   groups = fem_pre_mesh_groups_create(mesh, group_defs, options.geo_tol);
 %!   mesh.groups.tria6 = groups.tria6;
-
 %!   cms_opt.modes.number = int32(options.number_of_modes);
 %!   cms_opt.nodes.modal.number = int32(rows(mesh.nodes) + 1);
 %!   cms_opt.nodes.interfaces(1).number = int32(rows(mesh.nodes) + 2);
@@ -901,18 +883,14 @@ endfunction
 %!   mesh.nodes(cms_opt.nodes.modal.number, :) = [0, 0, param.o, 0, 0, 0];
 %!   mesh.nodes([cms_opt.nodes.interfaces.number], :) = [0, 0, -0.5 * param.l, 0, 0, 0;
 %!                                                       0, 0,  0.5 * param.l, 0, 0, 0];
-
 %!   mesh.elements.rbe3(2) = fem_pre_mesh_rbe3_from_surf(mesh, 1, cms_opt.nodes.interfaces(1).number);
 %!   mesh.elements.rbe3(3) = fem_pre_mesh_rbe3_from_surf(mesh, 2, cms_opt.nodes.interfaces(2).number);
 %!   mesh.elements.rbe3(1) = fem_pre_mesh_rbe3_from_surf(mesh, 3, cms_opt.nodes.modal.number);
-
 %!   load_case.locked_dof = false(rows(mesh.nodes), columns(mesh.nodes));
-
 %!   mesh.materials.tet10 = zeros(rows(mesh.elements.tet10), 1, "int32");
 %!   mesh.materials.tet10(mesh.groups.tet10(find([mesh.groups.tet10.id == 1])).elements) = 1;
 %!   mesh.material_data.rho = param.rho;
 %!   mesh.material_data.C = fem_pre_mat_isotropic(param.E, param.nu);
-
 %!   fprintf(stderr, "building cms element ...\n");
 %!   [mesh, mat_ass, dof_map, sol_eig] = fem_cms_create(mesh, load_case, cms_opt);
 %!   for i=1:numel(sol_eig.f)
@@ -920,31 +898,23 @@ endfunction
 %!       fprintf(stderr, "mode %d: %.1fHz\n", i, sol_eig.f(i) * (1 / SI_unit_second));
 %!     endif
 %!   endfor
-
 %!   mesh_post_pro_file = sprintf("%s_post.msh", filename);
-
 %!   fem_post_mesh_export(mesh_post_pro_file, mesh);
-
 %!   for j=1:numel(sol_eig.f)
 %!     eig_post_pro_file_mode{j} = sprintf("%s_eig_def_%03d.msh", filename, j);
 %!     fem_post_sol_step_export(eig_post_pro_file_mode{j}, sol_eig, j, j, sol_eig.f(j), options.scale_def / max(norm(sol_eig.def(:, 1:3, j), "rows")));
 %!   endfor
-
 %!   eig_post_pro_file = sprintf("%s_modes_post.geo", filename);
 %!   fd = -1;
 %!   unwind_protect
 %!     [fd, msg] = fopen(eig_post_pro_file, "w");
-
 %!     if (fd == -1)
 %!       error("failed to open file \"%s\"", eig_post_pro_file);
 %!     endif
-
 %!     fprintf(fd, "Merge \"%s\";\n", mesh_post_pro_file);
-
 %!     for j=1:numel(eig_post_pro_file_mode)
 %!       fprintf(fd, "Merge \"%s\";\n", eig_post_pro_file_mode{j});
 %!     endfor
-
 %!     fputs(fd, "View.Type = 1;\n");
 %!     fputs(fd, "View.VectorType = 5;\n");
 %!     fputs(fd, "View.Visible = 1;\n");
@@ -958,10 +928,8 @@ endfunction
 %!       fclose(fd);
 %!     endif
 %!   end_unwind_protect
-
 %!   mat_ass.Dred = param.alpha * mat_ass.Mred + param.beta * mat_ass.Kred;
 %!   fem_cms_export([filename, "_cms"], mesh, dof_map, mat_ass, cms_opt);
-
 %!   for i=1:numel(mbdyn_filenames)
 %!     options_mbd(i).output_file = sprintf("%s_%d", filename, i);
 %!     options_mbd(i).mbdyn_command = options.mbdyn_command;
@@ -978,9 +946,7 @@ endfunction
 %!     [res(i).t, res(i).trajectory, res(i).deformation, res(i).velocity, res(i).acceleration, res(i).node_id] = mbdyn_post_load_output_struct(options_mbd(i).output_file);
 %!     res(i).log_dat.vars = mbdyn_post_id_to_index(res(i), res(i).log_dat.vars);
 %!   endfor
-
 %!   omega_crit = zeros(1, numel(mbdyn_filenames));
-
 %!   for i=1:numel(mbdyn_filenames)
 %!     dm = res(i).log_dat.vars.dm;
 %!     dr = res(i).log_dat.vars.dr;
@@ -994,23 +960,22 @@ endfunction
 %!     f = m1 * g * l^3 / (48 * E * Iy);
 %!     omega_crit(i) = sqrt(g / f);
 %!   endfor
-
+%!   r = omega = cell(1, numel(mbdyn_filenames));
 %!   figure("visible", "off");
 %!   hold on;
-
 %!   for i=1:numel(mbdyn_filenames)
-%!     omega = res(i).velocity{res(i).log_dat.vars.node_idx_rotor}(:, 6);
-%!     r = norm(res(i).deformation{res(i).log_dat.vars.node_idx_rotor}(:, 1:2), "rows");
-
-%!     plot(omega * 30 / pi * (1 / SI_unit_second), 1e3 * r * SI_unit_meter, sprintf("-;%s;%d", printable_title(mbdyn_filename_suffix{i}), i));
+%!     omega{i} = res(i).velocity{res(i).log_dat.vars.node_idx_rotor}(:, 6);
+%!     r{i} = norm(res(i).deformation{res(i).log_dat.vars.node_idx_rotor}(:, 1:2), "rows");
+%!     plot(omega{i} * 30 / pi * (1 / SI_unit_second), 1e3 * r{i} * SI_unit_meter, sprintf("-;%s;%d", printable_title(mbdyn_filename_suffix{i}), i));
 %!   endfor
-
 %!   xlabel("n [rpm]");
 %!   ylabel("r [mm]");
 %!   grid on;
 %!   grid minor on;
 %!   title("resonance curve center of disk versus speed - magnitude");
 %!   figure_list();
+%!   tol = 10e-2;
+%!   assert(interp1(omega{2}, r{2}, omega{1}, "pchip", "extrap"), r{1}, tol * max(abs(r{1})));
 %! unwind_protect_cleanup
 %!   if (numel(filename))
 %!     fn = dir([filename, "*"]);
