0860eed80351a033d2d17393c64c9a083fe379c2
Reinhard <octave-user@a1.net>
2024-12-11 08:09:44 +0100

    Squashed commit of the following:
    commit 4d753bc3b4d2b31896aa2a84ce68eabf458d6632
    Author: Reinhard <octave-user@a1.net>
    Date:   Tue Dec 10 17:30:30 2024 +0100
    
        Add output of lambda and zeta

--- inst/fem_sol_modes_scale2.m
+++ inst/fem_sol_modes_scale2.m
@@ -33,7 +33,7 @@
 ## @seealso{fem_sol_harmonic_modal2, fem_sol_modal}
 ## @end deftypefn
 
-function [dgen, kgen, rgen] = fem_sol_modes_scale2(M, D, K, Phi, R)
+function [dgen, kgen, rgen, lambda, zeta] = fem_sol_modes_scale2(M, D, K, Phi, R)
   mgen = dgen = kgen = zeros(1, columns(Phi));
 
   for i=1:columns(Phi)
@@ -49,4 +49,30 @@ function [dgen, kgen, rgen] = fem_sol_modes_scale2(M, D, K, Phi, R)
   if (nargout >= 3)
     rgen = diag(1 ./ mgen) * (Phi.' * R);
   endif
+
+  if (nargout > 3)
+    lambda = -dgen / 2 - sqrt((dgen / 2).^2 - kgen);
+
+    zeta = -real(lambda) ./ abs(lambda);
+  endif
 endfunction
+
+%!test
+%! M = 10;
+%! K = 500;
+%! D = 30;
+%! Phi = 1;
+%! R = 1;
+%! zeta_ref = 1 / 2 * D / sqrt(K * M);
+%! [dgen, kgen, rgen, lambda, zeta] = fem_sol_modes_scale2(M, D, K, Phi, R);
+%! assert(zeta, zeta_ref);
+
+%!test
+%! tan_delta = 0.01e-2;
+%! M = 10;
+%! K = 500 * (1 + 1j * tan_delta);
+%! D = 0;
+%! Phi = 1;
+%! R = 1;
+%! [dgen, kgen, rgen, lambda, zeta] = fem_sol_modes_scale2(M, D, K, Phi, R);
+%! assert(2 * zeta, tan_delta, sqrt(eps) * zeta);
